c5d17953b68ae2016c0703731ee7bcd202e18bd3
Paulo Henrique <paulohr.abreu@gmail.com>
2020-01-30 00:14:59 -0200

    Modificar Wallet Schema (max deposit and max withdraw).

--- src/db/schemas/wallet.js
+++ src/db/schemas/wallet.js
@@ -10,6 +10,8 @@ WalletSchema.prototype.schema = {
     playBalance                 : { type: Number, required : true, default : 0},
     currency                    : { type : mongoose.Schema.Types.ObjectId, ref: 'Currency', required : true },
     bank_address                : { type: String, default : 0}, //Only Need on App
+    max_deposit                 : { type: Number, default: 0},
+    max_withdraw                : { type: Number, default: 0}
 }
 
 WalletSchema.prototype.model = db.model(WalletSchema.prototype.name, new db.Schema(WalletSchema.prototype.schema));
