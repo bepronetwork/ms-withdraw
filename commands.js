e6b8dda1efda64c7c7f24b3afbb9cea541eea399
Paulo <paulohr.abreu@gmail.com>
2020-12-08 12:14:39 -0300

    command to modify address auto wallet

--- commands.js
+++ commands.js
@@ -1,12 +1,12 @@
-const subCommands = process.argv.slice(2)[0].split("=");
+const subCommands = process.argv.slice(2);
 
-switch (subCommands[0]) {
-    case "BTCWalletId": {
+switch (subCommands[0].toUpperCase()) {
+    case "BTC": {
         const start = require("./src/commands/setAutoWallet.js");
         start(subCommands[1], "btc");
         break;
     }
-    case "ETHWalletId": {
+    case "ETH": {
         const start = require("./src/commands/setAutoWallet.js");
         start(subCommands[1], "eth");
         break;
